d3ffa51b8aa0b9d5a2604dfeef28a08578abc0f4
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-05-19 17:49:20 -0700

    display profile

--- Github_Finder/app.js
+++ Github_Finder/app.js
@@ -1,5 +1,7 @@
 // Init Github
 const github = new Github;
+// Init UI
+const ui = new UI;
 
 // Search Input
 const searchUser = document.getElementById("searchUser");
@@ -19,6 +21,7 @@ searchUser.addEventListener('keyup', (e) => {
       }
       else{
         // Show profile
+        ui.showProfile(data.profile)
 
       }
     })
