79bd2110a5fb0b371fb66498a47ed3440f0dab80
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-05-19 20:57:21 -0700

    show alert messages

--- Github_Finder/ui.js
+++ Github_Finder/ui.js
@@ -31,4 +31,42 @@ class UI{
       <div id="repos"></div>
     `;
   }
+
+  // Show alert message
+  showAlert(message, className){
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div = document.createElement('div');
+
+    // Add class
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+
+    // Get parent
+    const container = document.querySelector('.searchContainer');
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    });
+  }
+
+  // Clear alert message
+  clearAlert(){
+    const currentAlert = document.querySelector('.alert');
+
+    if (currentAlert){
+      currentAlert.remove();
+    }
+  }
+
+  // Clear Profile
+  clearProfile(){
+    this.profile.innerHTML = '';
+  }
 }
